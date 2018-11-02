79a46162a30a85a79c8c8aabc9a763da4783aab6
Silatman3 <32736545+Silatman3@users.noreply.github.com>
2018-11-02 15:04:52 +0000

    Add files via upload

--- Week4 Q10.py
+++ Week4 Q10.py
@@ -1,21 +1,27 @@
 from statistics import mean
 
 
-first_sweet = float(input('Enter the price of the first sweet:    '))
-second_sweet = float(input('Enter the price of the second sweet:   '))
-third_sweet = float(input('Enter the price of the third sweet:    '))
-fourth_sweet = float(input('Enter the price of the fourth sweet:   '))
-fifth_sweet = float(input('Enter the price of the fifth sweet:    '))
+first_sweet = input('Enter the price of the first sweet:    ')[:-1]
+second_sweet = input('Enter the price of the second sweet:   ')[:-1]
+third_sweet = input('Enter the price of the third sweet:    ')[:-1]
+fourth_sweet = input('Enter the price of the fourth sweet:   ')[:-1]
+fifth_sweet = input('Enter the price of the fifth sweet:    ')[:-1]
 print()
 
+first_sweet = int(first_sweet)
+second_sweet = int(second_sweet)
+third_sweet = int(third_sweet)
+fourth_sweet = int(fourth_sweet)
+fifth_sweet = int(fifth_sweet)
+
 total_price = first_sweet + second_sweet + third_sweet + fourth_sweet + fifth_sweet
-print('Total price:     ', + total_price)
+print('Total price: ', total_price)
 
 average_price = (first_sweet, second_sweet, third_sweet, fourth_sweet, fifth_sweet)
-print('Average price:   ', mean(average_price))
+print('Average price: ', mean(average_price))
 
 highest_price = max(average_price)
-print('Highest price:   ', highest_price)
+print('Highest price: ', highest_price)
 
 lowest_price = min(average_price)
-print('Lowest price:    ', lowest_price)
+print('Lowest price: ', lowest_price)
