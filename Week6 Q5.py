da7a19cc2e7c057d384b9fdd0bbf3054bfe38ff3
Silatman3 <32736545+Silatman3@users.noreply.github.com>
2018-11-08 17:09:04 +0000

    Add files via upload

---
+++ Week6 Q5.py
@@ -0,0 +1,42 @@
+"""
+
+Program that calculates various statistics of interest relating to a footballs team's performance
+
+"""
+
+team_name = str(input('Enter the Teams Name: '))
+print(team_name)
+
+game_one_scored = int(input('Enter the number of goals your team scored in Game #1: '))
+print(team_name, 'scored', game_one_scored, 'goals in Game #1!')
+game_one_conceded = int(input('Enter the number of goals your team conceded in Game #1: '))
+print(team_name, 'conceded', game_one_conceded, 'goals in Game #1!')
+
+print()
+
+game_two_scored = int(input('Enter the number of goals your team scored in Game #2: '))
+print(team_name, 'scored', game_two_scored, 'goals in Game #2!')
+game_two_conceded = int(input('Enter the number of goals your team conceded in Game #2: '))
+print(team_name, 'conceded', game_two_conceded, 'goals in Game #2!')
+
+print()
+
+game_three_scored = int(input('Enter the number of goals your team scored in Game #3: '))
+print(team_name, 'scored', game_three_scored, 'goals in Game #3!')
+game_three_conceded = int(input('Enter the number of goals your team conceded in Game #3: '))
+print(team_name, 'conceded', game_three_conceded, 'goals in Game #3!')
+
+print()
+
+game_four_scored = int(input('Enter the number of goals your team scored in Game #4: '))
+print(team_name, 'scored', game_four_scored, 'goals in Game #4!')
+game_four_conceded = int(input('Enter the number of goals your team conceded in Game #4: '))
+print(team_name, 'conceded', game_four_conceded, 'goals in Game #4!')
+
+print()
+
+game_five_scored = int(input('Enter the number of goals your team scored in Game #5: '))
+print(team_name, 'scored', game_five_scored, 'goals in Game #5!')
+game_five_conceded = int(input('Enter the number of goals your team conceded in Game #5: '))
+print(team_name, 'conceded', game_five_conceded, 'goals in Game #5!')
+
